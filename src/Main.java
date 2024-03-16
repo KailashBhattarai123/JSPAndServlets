777c9bd17c20d6f1142a16004166cbc7932faad5
KailashBhattarai123 <bhattarai.kailash1998@gmail.com>
2024-03-16 10:27:22 +0545

    First commit

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("So you think you can change?");
+        System.out.println("How are you man?");
     }
 }
