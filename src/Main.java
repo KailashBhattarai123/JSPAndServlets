53cb62921069934c07683df344dabb7e815acb0b
KailashBhattarai123 <bhattarai.kailash1998@gmail.com>
2024-03-16 17:43:08 +0545

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("So you think you can change?");
-        System.out.println("How are you man?");
 
     }
 }
---
+++ src/web/adddoctors.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hello JSP</title>
+</head>
+<body>
+    <h1>Hello, this is a JSP page!</h1>
+    <p>Welcome to the JSP page.</p>
+
+
+</body>
+</html>
